1eabbffb4f4f8014ae73b85d44e06c08ed158539
Sudhanva Bhattar <sudhanva.acharya711@gmail.com>
2026-05-15 23:33:55 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 9
+print(type(variable))
+variable = 9.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "asdfjkl;"
+print(type(variable))
+variable = [1,2,3,4,5]
+print(type(variable))
